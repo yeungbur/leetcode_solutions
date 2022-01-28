e5fb8170c33ee5c42c948c6771051354e362e662
yeungbur <63782705+yeungbur@users.noreply.github.com>
2022-01-28 03:59:53 -0500

    Add solutions

---
+++ medium/176_Second_Highest_Salary.sql
@@ -0,0 +1,14 @@
+/*
+
+176. Second Highest Salary
+
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+
+*/
+
+-- Solution 1, Simple
+
+SELECT MAX(salary) as SecondHighestSalary
+FROM employee
+WHERE salary NOT IN (SELECT MAX(Salary) from employee)
+;
